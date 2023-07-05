6c936ab1d88bb0b4c6a2ecc2d8f0768e68198c91
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-05 22:04:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,44 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstname, setFirstname] = useState('')
+  const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
 function handleSubmit(e) {
   e.preventDefault()
-  console.log('submitted');
-  console.log(e);
+  if (firstname && email) {
+    const person = {firstname, email}
+    setPeople(people =>{
+      return [...people, person]
+    });
+    // console.log(person);
+    console.log(people);
+  }
+  setFirstname('')
+  setEmail('')
 }
-
   return (
     <>
       <form action="#" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="firstname">Name : </label>
-          <input type="text" id="firstname" name="firstname" />
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+          />
         </div>
         <div className="form-control" style={{ marginTop: "20px" }}>
           <label htmlFor="email">Email : </label>
-          <input type="text" id="email" name="email" />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <button
           type="submit"
