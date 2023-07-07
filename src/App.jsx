d2a0f0107e70315138f87007985d1576f2e32f2f
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-07 14:59:58 +0100

    update App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
-
+const books = [
+  {
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    genre: "Fiction",
+    year: 1988,
+  },
+  {
+    title: "Sapiens: A Brief History of Humankind",
+    author: "Yuval Noah Harari",
+    genre: "Non-fiction",
+    year: 2011,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Fiction",
+    year: 1960,
+  },
+  {
+    title: "The Power of Now",
+    author: "Eckhart Tolle",
+    genre: "Self-help",
+    year: 1997,
+  },
+];
+  const text = 'the'
+  const filter = books.filter((ele) => {
+ return  ele.title.toLowerCase().includes(text)
+  });
+console.log(filter);
 function App() {
+
   return (
-    <div>App</div>
+    <div>
+      {
+        filter.map(ele =>{
+          const {title, author, genre, year} = ele
+          return (
+            <div>
+              <h1>{title}</h1>
+              <h3>{author}</h3>
+              <p>{genre}</p>
+              <span>{year}</span>
+            </div>
+          );
+        })
+      }
+    </div>
   )
 }
 
